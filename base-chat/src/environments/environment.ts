19035e3105716a351b180047dcdce3829e4c9e86
Tarik Nasraoui <tnasraoui@techmyteam.com>
2017-10-18 17:32:06 +0100

    evolution

--- base-chat/src/environments/environment.ts
+++ base-chat/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+		apiKey: "AIzaSyDFffdmZs0JuDgyhTWOvJ-AxKJi3Eb5hrQ",
+		authDomain: "myfirstbase-8c767.firebaseapp.com",
+		databaseURL: "https://myfirstbase-8c767.firebaseio.com",
+		projectId: "myfirstbase-8c767",
+		storageBucket: "myfirstbase-8c767.appspot.com",
+		messagingSenderId: "930106293366"
+  }
 };
