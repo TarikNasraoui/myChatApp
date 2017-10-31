9fe9aff947bfe2c238a3ab1e9a30767e705647c2
Tarik Nasraoui <tnasraoui@techmyteam.com>
2017-10-31 17:50:52 +0000

    evolution

--- base-chat/src/app/services/chat.service.ts
+++ base-chat/src/app/services/chat.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, firebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import { ChatMessage } from '../models/chat-message.model';
+import { AuthService } from '../services/auth.service';
+import * as firebase from 'firebase/app';
+
+
+
+
+
 
 @Injectable()
 export class ChatService {
