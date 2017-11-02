78e09cdea6b7a92e979a7e301854e946c1f817f1
Tarik Nasraoui <tnasraoui@techmyteam.com>
2017-11-02 18:00:38 +0000

    chatapp

---
+++ base-chat/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+   firebase: {
+	  	apiKey: "AIzaSyDFffdmZs0JuDgyhTWOvJ-AxKJi3Eb5hrQ",
+	    authDomain: "myfirstbase-8c767.firebaseapp.com",
+	    databaseURL: "https://myfirstbase-8c767.firebaseio.com",
+	    projectId: "myfirstbase-8c767",
+	    storageBucket: "myfirstbase-8c767.appspot.com",
+	    messagingSenderId: "930106293366"
+  }
+};
