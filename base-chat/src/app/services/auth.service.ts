020996e2fcd81ed93ee3579162f2755e437c7af7
Tarik Nasraoui <tnasraoui@techmyteam.com>
2017-11-22 17:59:11 +0000

    second step begin

--- base-chat/src/app/services/auth.service.ts
+++ base-chat/src/app/services/auth.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router'; 
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
+import { User } from '../models/user.model';
+
+
 
 @Injectable()
 export class AuthService {
+  private user : Observable<firebase.user>;
+  private authState : any;
 
-  constructor() { }
+  constructor(
+  	private afAuth : AngularFireAuth,
+  	private db: AngularFireDatabse,
+  	private router : Router 
+  	) { 
+  		this.user = afAuth.authState;
+  	}
+  	signUp(email: string, password: string, displayName: string) {
+  		return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+  		.then((user)=>{
+  			
+  		})
+  	}
 
 }
